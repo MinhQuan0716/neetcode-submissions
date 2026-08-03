6626025df0f48fcbc8c38d9ad8f989d33c1b4e9e
Trần Trịnh Minh Quân <107088923+MinhQuan0716@users.noreply.github.com>
2026-08-03 10:47:16 +0700

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<List<int>> ThreeSum(int[] nums) {
+        int n = nums.Length;
+        Array.Sort(nums);
+        List<List<int>> result = new List<List<int>>();
+        for (int i = 0; i < n - 2; i++)
+{
+    if (i > 0 && nums[i] == nums[i - 1]) continue;
+    int left = i + 1, right = n - 1;
+    while (left < right)
+    {
+        int sum = nums[i] + nums[left] + nums[right];
+       if(sum == 0){
+        result.Add(new List<int> { nums[i], nums[left], nums[right] });
+        left++;
+        right--;
+         while (left < right && nums[left] == nums[left - 1]) left++;
+         while (left < right && nums[right] == nums[right + 1]) right--;
+       } else if(sum <0){
+         left++;
+       } else{
+        right--;
+       }
+    }
+}
+    return result;
+    }
+}
